37c30f1ec8b902303341f72dc894de92f88013e1
Muthukumar Arumugam <muthukumara@outlook.com>
2018-05-14 14:40:34 +0530

    example committed

---
+++ AutofacWpf/BootStrapper.cs
@@ -0,0 +1,58 @@
+﻿using Autofac;
+using Autofac.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutofacWpf
+{
+    public static class BootStrapper
+    {
+        private static ILifetimeScope _rootScope;
+        
+        public static void Start()
+        {
+            if (_rootScope != null)
+            {
+                return;
+            }
+
+            var builder = new ContainerBuilder();
+            builder.RegisterType<DataService>()
+                 .As<IDataService>()
+                 .InstancePerRequest();
+
+            _rootScope = builder.Build();
+        }
+
+        public static void Stop()
+        {
+            _rootScope.Dispose();
+        }
+
+        public static T Resolve<T>()
+        {
+            if (_rootScope == null)
+            {
+                throw new Exception("Bootstrapper hasn't been started!");
+            }
+
+            return _rootScope.Resolve<T>(new Parameter[0]);
+        }
+
+        public static T Resolve<T>(Parameter[] parameters)
+        {
+            if (_rootScope == null)
+            {
+                throw new Exception("Bootstrapper hasn't been started!");
+            }
+
+            return _rootScope.Resolve<T>(parameters);
+        }
+    }
+
+   
+}
---
+++ AutofacWpf/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutofacWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+       
+    }
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel(IDataService dataService)
+        {
+
+        }
+    }
+    public interface IDataService
+    {
+        bool Data { get; set; }
+    }
+
+    public class DataService : IDataService
+    {
+        public bool Data { get; set; } = true;
+    }
+}
